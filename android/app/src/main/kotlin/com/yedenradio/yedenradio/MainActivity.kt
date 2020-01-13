ca9a1852f1fe11dde82c13fdb930f21998e34433
Brusd <brusd.com@gmail.com>
2020-01-13 23:48:35 +0200

    init project

---
+++ android/app/src/main/kotlin/com/yedenradio/yedenradio/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.yedenradio.yedenradio
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
